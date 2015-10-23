4937b37d4a34d69fba301487eb94c93bfbe62dd7
Scott Davis <Scott.Davis@Webtrends.com>
2015-10-23 01:35:23 -0700

    basic guages working.

--- public/javascripts/stream.js
+++ public/javascripts/stream.js
@@ -1,12 +1,23 @@
+
+var keg_size = 19500;  //milliters in a sixth barrel keg
+
 var options = {
   width: "400", height: "50%",
-  redFrom: 0, redTo: 10,
-  yellowFrom:10, yellowTo: 30,
-  minorTicks: 5
+  redFrom: 0, redTo: keg_size * 0.10,
+  yellowFrom: keg_size * 0.10, yellowTo: keg_size * 0.30,
+  minorTicks: 5,
+  max: keg_size
 };
 
-google.load("visualization", "1", {packages:["gauge"]});
 
+
+function normalize(val){
+  // return parseInt((val / keg_size ) * 100);
+  return parseInt(val);
+}
+
+
+google.load("visualization", "1", {packages:["gauge"]});
 var source = new EventSource('/stream');
 source.addEventListener('message', function(e) {
   
@@ -15,14 +26,14 @@ source.addEventListener('message', function(e) {
 
   var d1 = google.visualization.arrayToDataTable([
      ['Keg', 'Mills'],
-     [data[0].color,  data[0].millis]
+     [data[0].color,  normalize(keg_size - data[0].millis)]
   ]);
   var chart1 = new google.visualization.Gauge(document.getElementById('orange_div'));
   chart1.draw(d1, options);
 
   var d2 = google.visualization.arrayToDataTable([
      ['Keg', 'Mills'],
-     [data[1].color,  data[1].millis]
+     [data[1].color,  normalize(keg_size - data[1].millis)]
   ]);
   var chart2 = new google.visualization.Gauge(document.getElementById('green_div'));
   chart2.draw(d2, options);
@@ -30,7 +41,7 @@ source.addEventListener('message', function(e) {
 
   var d3 = google.visualization.arrayToDataTable([
      ['Keg', 'Mills'],
-     [data[2].color,  data[2].millis]
+     [data[2].color,  normalize(keg_size - data[2].millis)]
   ]);
   var chart3 = new google.visualization.Gauge(document.getElementById('yellow_div'));
   chart3.draw(d3, options);
