14cb06de4e830295728b4c8ec57a5860ba368024
Scott Davis <Scott.Davis@Webtrends.com>
2015-10-23 09:47:05 -0700

    added dcsid for collection.

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ export USER_ID="TBD"
 export ACCOUNT_ID="TBD"
 export CLIENT_ID="TBD"
 export CLIENT_SECRET="TBD"
-export DCSID="TBD"
-export LOCAL_ONLY=true #do not send to collection
+export DCSID="dcswxkx6r100004rra9vt8h0n_9g3z"  
+# export LOCAL_ONLY=true #do not send to collection
 #export LIVE=true  #use gpio to read data
 
 
