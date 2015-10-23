eb2af54b1e914a706465a98ddf888331327b59ab
Scott Davis <Scott.Davis@Webtrends.com>
2015-10-23 10:39:20 -0700

    runnin'

--- run.sh
+++ run.sh
@@ -5,8 +5,7 @@ export ACCOUNT_ID="TBD"
 export CLIENT_ID="TBD"
 export CLIENT_SECRET="TBD"
 export DCSID="dcswxkx6r100004rra9vt8h0n_9g3z"  
-# export LOCAL_ONLY=true #do not send to collection
+#export LOCAL_ONLY=true #do not send to collection
 #export LIVE=true  #use gpio to read data
-
 
 npm start
