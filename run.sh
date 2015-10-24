d3ea67cb7f2731e2389eba92fe5a75ba43b508ac
Scott Davis <Scott.Davis@Webtrends.com>
2015-10-24 13:16:41 -0700

    set live default for run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 #export LOCAL_ONLY=true #do not send to collection
 export DCSID="dcswxkx6r100004rra9vt8h0n_9g3z"  
-#export LIVE=true  #use gpio to read data
+export LIVE=true  #use gpio to read data
 
 npm start
